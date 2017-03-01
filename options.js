468a98dd1083bbbc9a56f1ac932a601ba2790d20
Tony Xie <tx248@nyu.edu>
2017-02-28 21:49:32 -0800

    options save, fix dropdown styles later

--- options.js
+++ options.js
@@ -1,9 +1,19 @@
 function save_options() {
-    var color = document.getElementById('color').value;
-    var likesColor = document.getElementById('like').checked;
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var phone = document.getElementById('phone').value;
+    var company = document.getElementById('company').value;
+    var linkedin = document.getElementById('linkedin').value;
+    var twitter = document.getElementById('twitter').value;
+    var website = document.getElementById('website').value;
     chrome.storage.sync.set({
-        favoriteColor: color,
-        likesColor: likesColor
+        name: name,
+        email: email,
+        phone: phone,
+        company: company,
+        linkedin: linkedin,
+        twitter: twitter,
+        website: website
     }, function() {
         // Update status to let user know options were saved.
         var status = document.getElementById('status');
@@ -19,11 +29,21 @@ function save_options() {
 function restore_options() {
     // Use default value color = 'red' and likesColor = true.
     chrome.storage.sync.get({
-        favoriteColor: 'red',
-        likesColor: true
+      name,
+      email,
+      phone,
+      company,
+      linkedin,
+      twitter,
+      website
     }, function(items) {
-        document.getElementById('color').value = items.favoriteColor;
-        document.getElementById('like').checked = items.likesColor;
+        document.getElementById('name').value = items.name;
+        document.getElementById('email').value = items.email;
+        document.getElementById('phone').value = items.phone;
+        document.getElementById('company').value = items.company;
+        document.getElementById('linkedin').value = items.linkedin;
+        document.getElementById('twitter').value = items.twitter;
+        document.getElementById('website').value = items.website;
     });
 }
 document.addEventListener('DOMContentLoaded', restore_options);
