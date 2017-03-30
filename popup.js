a071795217cf24117d53cba96da642340aa12f0c
yongpark <yongpark.sd@gmail.com>
2017-03-29 22:42:28 -0700

    adds chome tab logic to popup.js

--- popup.js
+++ popup.js
@@ -116,3 +116,19 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   });
 });
+
+
+chrome.extension.onRequest.addListener(
+  function(request, sender, sendResponse) {
+    // LOG THE CONTENTS HERE
+    console.log(request.content);
+  });
+
+chrome.tabs.getSelected(null, function(tab) {
+
+  // Now inject a script onto the page
+  chrome.tabs.executeScript(tab.id, {
+       code: "chrome.extension.sendRequest({content: document.body.innerHTML}, function(response) { console.log('success'); });"
+     }, function() { console.log('done'); });
+
+});
