00e82123b319860a43c6600ede84bdd04b3d6f5f
yongpark <yongpark.sd@gmail.com>
2017-03-31 16:34:03 -0700

    creates input dom manipulation logic for name. assigns vars to the rest of the inputs

--- background.js
+++ background.js
@@ -1,6 +1,18 @@
 chrome.storage.sync.get({
   name
 }, function(items) {
-  console.log(items);
+  let name = items.name;
+  let email = items.email;
+  let phone = items.phone;
+  let company = items.company;
+  let linkedin = items.linkedin;
+  let twitter = items.twitter;
+  let website = items.website;
+  let gender = items.gender;
+  let race = items.race;
+  let veteran = items.veteran;
+  let disability = items.disability;
+  console.log(name);
+  $( "input[name='name']" ).val(name);
 });
 $("body").append('Test');
